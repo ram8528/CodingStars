946ba7710abb552e84e28ab9c0c28ea9d68a1df9
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-12-27 21:26:52 +0530

    final

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -10,7 +10,9 @@ export const AppContent = createContext();
 export const AppContextProvider = (props) => {
 
     axios.defaults.withCredentials = true;
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  // const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://coding-stars-nove.vercel.app";
+
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(null);
 

9b4ff51d05559ec4eb764247e1cdf5b2c344db8c
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-12-29 21:27:40 +0530

    `Added Link to feedback page in Home component`

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
         <Navbar/>
         <Header/>
+        <Link to="/feedback">Feedback</Link>
     </div>
   )
 }

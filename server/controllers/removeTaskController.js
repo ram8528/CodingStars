564293cdaffe3f82827e15b6c982a53886ca9e9c
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-12-30 21:45:41 +0530

    `Refactored removeTaskController.js to fix formatting and add newline at end of file`

--- server/controllers/removeTaskController.js
+++ server/controllers/removeTaskController.js
@@ -1,32 +1,41 @@
 import { TaskModel } from "../models/taskModel.js";
 import userModel from "../models/userModel.js";
 
-export const removeTask = async(req,res) => {
-    try {
-        const result  = await TaskModel.findOneAndDelete(({
-            userId : req.userId,
-            _id : req.body.taskId,
-        }));
-        if(result) {
-            const user = await userModel.findOneAndUpdate
-            ({
-                _id : req.userId
-            }, {
-                $pull : {
-                    tasks : req.body.taskId,
-                }
-            });
-            return res.status(200).json({
-                success: true,
-                message: "Task Deleted Successfully",
-                list : null
-            });
+export const removeTask = async (req, res) => {
+  try {
+    const result = await TaskModel.findOneAndDelete({
+      userId: req.userId,
+      _id: req.body.taskId,
+    });
+    if (result) {
+      await userModel.findOneAndUpdate(
+        {
+          _id: req.userId,
+        },
+        {
+          $pull: {
+            tasks: req.body.taskId,
+          },
         }
-    } catch (error) {
-        return res.status(401).json({
-            success: false,
-            message: 'Could Not Deleted',
-            taskList: null
-        })
+      );
+
+      const updatedUser = await userModel
+        .findById(req.userId)
+        .populate("tasks");
+
+      const taskList = updatedUser.tasks.length > 0 ? updatedUser.tasks : null;
+
+      return res.status(200).json({
+        success: true,
+        message: "Task Deleted Successfully",
+        list: taskList,
+      });
     }
-}
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Could Not Deleted",
+      taskList: null,
+    });
+  }
+};

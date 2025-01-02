caae69abd16c292da1a02ea5cb40f498c3a91e75
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-01-02 17:30:53 +0530

    "Update backend URL in AppContextProvider"

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,7 +14,7 @@ export const AppContextProvider = (props) => {
   const backendUrls = [
     import.meta.env.VITE_BACKEND_URL,
     // "https://coding-stars-nove.vercel.app",
-    "https://codingstars.onrender.com/",
+    "https://codingstars.onrender.com",
   ];
 
   const backendUrl = backendUrls.find((url) => url);
